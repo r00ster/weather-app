5496494f1a6dbfa09b455925481e05dd09759e52
Niel Roos <niel.r@superbalist.com>
2018-08-21 19:40:12 +0200

    Fix endpoint URL

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,7 +12,7 @@ export class DataService {
     // TODO: To prevent API key abuse, you should set up a proxy server to make calls to our API behind the scenes.
     // Using CORS-anywhere for now to prevent CORS issues
     this.corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
-    this.capeTownWeatherData = 'https://pi.darksky.net/forecast/bdbdf1842d3fbddd6e9dc57493177247/-33.9249,18.4241?units=si';
+    this.capeTownWeatherData = 'https://api.darksky.net/forecast/bdbdf1842d3fbddd6e9dc57493177247/-33.9249,18.4241?units=si';
   }
 
   getCapeTownWeather() {
